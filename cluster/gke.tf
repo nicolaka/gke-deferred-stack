60953646c7ca06401413f1725282a6845536bc58
Alex Somesan <alex.somesan@gmail.com>
2024-08-21 17:18:57 +0200

    Remove unsupported attribute on cluster resource.

--- cluster/gke.tf
+++ cluster/gke.tf
@@ -62,7 +62,7 @@ resource "google_container_cluster" "default" {
     machine_type = "n1-standard-4"
   }
 
-  deletion_protection = false
+  # deletion_protection = false
 }
 
 resource "local_file" "google_token" {
